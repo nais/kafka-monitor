72df75cb7d990512f4cc839aee80752f10c64f9d
Terje Sannum <terje.sannum@nav.no>
2018-11-01 16:07:57 +0100

    configurable log level

--- docker/start.sh
+++ docker/start.sh
@@ -12,6 +12,7 @@ if test ! -z "$JOLOKIA_URL"; then
    perl -pi -e 's/(.*new Jolokia\(\{url:\s*\").*?(\"\}.*)/\1$ENV{JOLOKIA_URL}\2/;' /kafka-monitor/webapp/index.html
 fi
 
+perl -p -e 's/\$\{(\w+)\}/$ENV{$1}/eg; s/.*:\s*\"\"\s*,?.*//;' < /kafka-monitor/config/log4j.properties.in > /kafka-monitor/config/log4j.properties
 perl -p -e 's/\$\{(\w+)\}/$ENV{$1}/eg; s/.*:\s*\"\"\s*,?.*//;' < /kafka-monitor/config/kafka-monitor.properties.in > /kafka-monitor/config/kafka-monitor.properties
 /kafka-monitor/bin/kafka-monitor-start.sh /kafka-monitor/config/kafka-monitor.properties
 
