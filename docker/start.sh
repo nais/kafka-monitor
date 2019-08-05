b8bea3878090379d4901cd084d897e1ea4c69a97
Terje Sannum <terje.sannum@nav.no>
2019-08-05 13:04:15 +0200

    upgrade to linkedin kafka-monitor 2.0.4

--- docker/start.sh
+++ docker/start.sh
@@ -8,11 +8,14 @@ if test ! -z "$JAAS_CONFIG"; then
     export JAAS_CONFIG="$(echo $JAAS_CONFIG | sed 's/"/\\"/g')"
 fi
 
+mkdir -p /config
+
 if test ! -z "$JOLOKIA_URL"; then
-   perl -pi -e 's/(.*new Jolokia\(\{url:\s*\").*?(\"\}.*)/\1$ENV{JOLOKIA_URL}\2/;' /kafka-monitor/webapp/index.html
+   perl -p -e 's/(.*new Jolokia\(\{url:\s*\").*?(\"\}.*)/\1$ENV{JOLOKIA_URL}\2/;' < /kafka-monitor/config/index.html.in > /config/index.html
 fi
 
-perl -p -e 's/\$\{(\w+)\}/$ENV{$1}/eg; s/.*:\s*\"\"\s*,?.*//;' < /kafka-monitor/config/log4j.properties.in > /kafka-monitor/config/log4j.properties
-perl -p -e 's/\$\{(\w+)\}/$ENV{$1}/eg; s/.*:\s*\"\"\s*,?.*//;' < /kafka-monitor/config/kafka-monitor.properties.in > /kafka-monitor/config/kafka-monitor.properties
-/kafka-monitor/bin/kafka-monitor-start.sh /kafka-monitor/config/kafka-monitor.properties
+perl -p -e 's/\$\{(\w+)\}/$ENV{$1}/eg; s/.*:\s*\"\"\s*,?.*//;' < /kafka-monitor/config/log4j.properties.in > /config/log4j.properties
+perl -p -e 's/\$\{(\w+)\}/$ENV{$1}/eg; s/.*:\s*\"\"\s*,?.*//;' < /kafka-monitor/config/kafka-monitor.properties.in > /config/kafka-monitor.properties
+
+KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:/config/log4j.properties" exec /kafka-monitor/bin/kafka-monitor-start.sh /config/kafka-monitor.properties
 
